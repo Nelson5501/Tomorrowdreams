9edb05a2f8d32adf7b240186ad86c149b867f146
Nelson5501 <nelsoncaterino@gmail.com>
2020-06-11 15:19:25 +0200

    problem with the bundle vich

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,11 +5,18 @@ namespace App\Entity;
 use App\service\ImageService;
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
-use vich\UploaderBundle\Mapping\Annotation as vich;
+use Doctrine\ORM\Mapping\colums;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+
 /**
  * @ORM\Entity(repositoryClass=PostRepository::class)
  * @Doctrine\ORM\Mapping\colums
- * @vich\uploadable
+ * @ORM\Column(type="string", length=250, nullable=false)
+ * @Vich\Uploadable
  */
  
 
@@ -19,6 +26,7 @@ class Post {
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Doctrine\ORM\Mapping\colums
      */
 
     private $id;
@@ -26,40 +34,72 @@ class Post {
     /**
      * @ORM\Column(type="string", length=255)
      */
+
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
+
     private $content;
 
     /**
-    * @ORM\colums(type="datetime")
+    * @ORM\colums(type="string", length=255)
     */
 
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
+     * @var File
+     */
+    
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="file")
+     * @var \DateTime
+     */
     private $updatedAt;
 
     /**
-    * @ORM\colums(type="boolean")
-    * @Doctrine\ORM\Mapping\colums
-    */
-   
-    private $thumbnail;
-
-    /**
-     * vich\uploadableFields(mappings="post_thumbnails", fileNameProperty="thumbnail")
+     * @ORM\Column(type="text")
+     * 
      */
-
-    private $thumbailFile;
-    
 
     private $slug;
 
     /**
     * @ORM\Column(type="string", length=255)
     */    
+    
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
 
-   
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
